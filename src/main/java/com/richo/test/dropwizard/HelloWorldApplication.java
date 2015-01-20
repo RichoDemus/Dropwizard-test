a7f90fab5fd0c175147180c67247f4cd304e2b29
Richard Tjerngren <richard.tjerngren@netnet.com>
2015-01-20 13:35:44 +0100

    Dropwizard-swagger doesnt work with dropwizard 0.8 yet

--- src/main/java/com/richo/test/dropwizard/HelloWorldApplication.java
+++ src/main/java/com/richo/test/dropwizard/HelloWorldApplication.java
@@ -10,11 +10,13 @@ import io.dropwizard.Application;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.federecio.dropwizard.swagger.SwaggerDropwizard;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.DispatcherType;
+import java.io.IOException;
 import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +26,7 @@ import java.util.Objects;
 public class HelloWorldApplication extends Application<HelloWorldConfiguration>
 {
 	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private final SwaggerDropwizard swaggerDropwizard = new SwaggerDropwizard();
 
 	public static void main(String[] args) throws Exception
 	{
@@ -41,6 +44,7 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration>
 	{
 		AssetsBundle assetsBundle = new AssetsBundle("/assets/", "/", "index.html", "static");
 		bootstrap.addBundle(assetsBundle);
+		swaggerDropwizard.onInitialize(bootstrap);
 	}
 
 	@Override
@@ -48,6 +52,15 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration>
 	                Environment environment)
 	{
 		enableWadl(environment);
+		try
+		{
+			swaggerDropwizard.onRun(configuration, environment);
+		}
+		catch (IOException e)
+		{
+			logger.error("Unable to initialize Dropwizard-swagger: {}", e.getMessage(), e);
+			throw new RuntimeException(e);
+		}
 
 		final HelloWorldApi resource = new HelloWorldResource(
 				configuration.getTemplate(),
